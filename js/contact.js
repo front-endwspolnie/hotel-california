11898bd9d6e02901f11d15f23739cda38bb4279f
htmlCssJsGrupa1 <36280247+htmlCssJsGrupa1@users.noreply.github.com>
2018-02-23 15:01:38 +0100

    git add cotact.html

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+/*function isNumber(valueToCheck)
+{
+    return !isNaN(valueToCheck);
+}*/
+
+window.onload = function(){
+     var name = document.getElementById("contact").name;
+     var email = document.getElementById("contact").name;
+     var message = document.getElementById("contact").name;
+     var submit = document.getElementById("contact").submit;
+     var isEverythingOK = true;
+
+          name.onchange = function(e){
+               if(name.value ===""){
+                    isEverythingOK = false;
+               }
+               else{
+                    isEverythingOK = true;
+               }
+               submit.onclick = function(e){
+                    if (!isEverythingOK)
+                    e.preventDefault();
+    };
+          };
